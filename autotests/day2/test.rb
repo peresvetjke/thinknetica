bac2586b6af54faa88d49d0f80364a73868e19ef
Victor Maslov <nakilon@gmail.com>
2022-11-03 11:51:28 +0300

    домашка

--- autotests/day2/test.rb
+++ autotests/day2/test.rb
@@ -3,8 +3,16 @@ minitest_cuprite "headless": false
 
 describe :test do
   it "test" do
-    visit "https://the-internet.herokuapp.com/login"
-    find_all("input")[0].set "qwe"
-    require "irb"; binding.irb
+    visit "http://automationpractice.com/index.php"
+
+    # найти нужные <a> с соответствующим текстом, чтобы перейти в раздел летних платьев
+    find(:xpath, "//???").hover    # Dresses
+    find(:xpath, "//???").click    # Summer Dresses
+
+    # убедиться, что отобразились три товара
+    assert_equal 3, find_all(:css,"???").size
+
+    # проверить цену платья Printed Chiffon
+    assert_equal "$16.40", find(:css,".product_list").find(:xpath,".//*[.//*[contains(text(),???)]]//*[???]").text
   end
 end
