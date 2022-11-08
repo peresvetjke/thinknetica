f0eb113bb40aa39a8cbe83d5ef94e513f61c5947
i.udalov <i.udalov@rambler-co.ru>
2022-11-08 09:32:01 +0300

    homework po pervomu dnyu

--- autotests/day1/directlink/lib/directlink.rb
+++ autotests/day1/directlink/lib/directlink.rb
@@ -1,4 +1,5 @@
 require "fastimage"
+require "faraday_middleware"
 
 module DirectLink
   class ErrorBadLink < RuntimeError ; end
@@ -13,9 +14,33 @@ module DirectLink
     end
   end
 
-  # def self.reddit link
-  # end
+  def self.reddit link
+    case link
+    when /\Ahttps?:\/\/.*reddit\.com\/.*/
+      connection = Faraday.new("#{link}.json") do |c|
+        c.use(FaradayMiddleware::FollowRedirects, limit: 10)
+        c.adapter(Faraday.default_adapter)
+      end
+      response = connection.get
+      json = JSON.parse(response.body)
+      raise ErrorBadLink.new link unless json.first.include? 'data'
 
+      get_reddit_image_params(json)
+    else
+      raise ErrorBadLink.new link
+    end
+  end
+
+  private
+
+  def self.get_reddit_image_params(json)
+    source_data = json.first['data']['children'].first['data']['preview']['images'].first['source']
+    image_url = source_data['url'].split('?').first  
+    image_width = source_data['width']
+    image_height = source_data['height']
+    image_format = image_url.scan(/\w+\z/).first
+    [image_url, image_width, image_height, image_format.to_sym]
+  end
 end
 
 def DirectLink link
@@ -23,6 +48,8 @@ def DirectLink link
   case host.split(?.).last(2)
   when %w{ imgur com }
     ::DirectLink.imgur link
+  when %w{ reddit com }
+    ::DirectLink.reddit link
   else
     raise ::DirectLink::ErrorBadLink.new link
   end
