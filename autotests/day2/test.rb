9ec9f3da860780d1d02b7bb38a415d5ddb76fd22
i.udalov <i.udalov@rambler-co.ru>
2022-11-10 10:24:30 +0300

    homework po vtoromu dnyu

--- autotests/day2/test.rb
+++ autotests/day2/test.rb
@@ -1,18 +1,32 @@
-require "minitest_cuprite"
-minitest_cuprite "headless": false
+# frozen_string_literal: true
+
+require 'minitest_cuprite'
+minitest_cuprite "headless": true
+
+def with_class(klass)
+  "[contains(concat(' ', normalize-space(@class), ' '), #{klass})]"
+end
 
 describe :test do
-  it "кол-во товаров в Summer Dresses и цена Printed Chiffon" do
-    visit "http://localhost:4567/index.php"
+  let(:dresses_xpath) { "//*#{with_class('menu-content')}/li/a[@title='Dresses']" }
+  let(:printed_chiffon_dress_price) do
+    find(:css, '.product_list')
+      .find(:xpath, "//*[contains(text(), 'Printed Chiffon Dress')]/../..\
+                     //div#{with_class('content_price')}/*[@itemprop='price']")
+      .text
+  end
 
-    # найти нужные <a> с соответствующим текстом, чтобы перейти в раздел летних платьев
-    find(:xpath, "//???").hover    # Dresses
-    find(:xpath, "//???").click    # Summer Dresses
+  before do
+    visit 'http://localhost:4567/index.php'
+    find(:xpath, dresses_xpath).hover # Dresses
+    find(:xpath, "#{dresses_xpath}/../ul//a[@title='Summer Dresses']").click # Summer Dresses
+  end
 
-    # убедиться, что отобразились три товара
-    assert_equal 3, find_all(:css,"???").size
+  it 'displays summer dressees list' do
+    assert_equal 3, find_all(:css, '.product_list li.ajax_block_product').size
+  end
 
-    # проверить цену платья Printed Chiffon
-    assert_equal "$16.40", find(:css,".product_list").find(:xpath,".//*[.//*[contains(text(),???)]]//*[???]").text
+  it 'displays correct price' do
+    assert_equal '$16.40', printed_chiffon_dress_price
   end
 end
